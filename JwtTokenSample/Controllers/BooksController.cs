5b771c1a5f78d28677716bf8f5d98b24723f34f4
Fatih Koçak <fatihkocak@hotmail.com>
2019-05-05 17:00:59 +0300

    ..

---
+++ JwtTokenSample/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace JwtTokenSample.Controllers
+{
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        [HttpGet, Authorize]
+        public IEnumerable<Book> Get()
+        {
+            var currentUser = HttpContext.User;
+            var resultBookList = new Book[] {
+                    new Book { Author = "Ray Bradbury",Title = "Fahrenheit 451" },
+                    new Book { Author = "Gabriel García Márquez", Title = "One Hundred years of Solitude" },
+                    new Book { Author = "George Orwell", Title = "1984" },
+                    new Book { Author = "Anais Nin", Title = "Delta of Venus" }
+                  };
+
+            return resultBookList;
+        }
+
+    }
+
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public bool AgeRestriction { get; set; }
+    }
+}
